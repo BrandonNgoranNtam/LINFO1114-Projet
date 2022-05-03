e42ea69b7a6ef544d0d7fb3aeb35738edd686aa7
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2022-05-03 15:53:21 +0200

    Base impl

---
+++ pageRankLinear.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def pageRankLinear(A: np.matrix, alpha: float, v: np.array) -> np.array:
+    """
+    Input: Une matrice d'adjacence A d'un graphe dirigé, pondéré et régulier G, un vecteur
+           de personnalisation v, ainsi qu'un paramètre de téléportation alpha compris entre
+           0 et 1 (0.9 par défaut et pour les résultats à présenter). Toutes ces valeurs sont
+           non-négatives.
+    Output: Un vecteur x contenant les scores d'importance des noeuds ordonnés dans
+            le même ordre que la matrice d'adjacence.
+    """
+
+    return np.array([])
