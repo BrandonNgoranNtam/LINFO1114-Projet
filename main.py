fb941d53ff4640ab445c3c2e98b59d810a0dfcc2
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2022-05-03 16:56:36 +0200

    main + matrix

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import numpy as np
+
+from pageRankLinear import pageRankLinear
+from pageRankPower import pageRankPower
+
+
+def read_csv(file) -> np.ndarray:
+    return np.loadtxt(file, delimiter=',')
+
+
+def print_adj_matrix_template():
+    for i in range(ord('A'), ord('J')+1):
+        for j in range(ord('A'), ord('J')+1):
+            print(f'{chr(i)}->{chr(j)}', end=',' if j != ord('J') else '')
+        print()
+
+
 def main():
-    pass
+    # print_adj_matrix_template()
+
+    vector = read_csv('VecteurPersonnalisation_Groupe32.csv')
+    matrix = np.matrix(read_csv('matrix.csv'))
+
+    print('vector:', vector)
+    print('matrix:', matrix)
+    print('number of edges in the graph:', np.count_nonzero(matrix))
+
+    print('pageRankLinear:', pageRankLinear(matrix, 0.9, vector))
+    print('pageRankPower:', pageRankPower(matrix, 0.9, vector))
 
 
 if __name__ == "__main__":
